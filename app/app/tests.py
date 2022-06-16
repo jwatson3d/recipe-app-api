37eaa8da6f4563e2171facb380aa65a72b575160
John Watson <jwatson3d@gmail.com>
2022-06-15 20:58:47 -0500

    add simple test case

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Simple tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Adding numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
